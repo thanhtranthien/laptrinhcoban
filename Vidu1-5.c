e0ed1ecdd0fe8bb29bb5af9b5da4a0dc6c414d42
thanhtranthien <50793089+thanhtranthien@users.noreply.github.com>
2020-10-31 20:13:23 +0700

    Create Vidu1-5.c

---
+++ Vidu1-5.c
@@ -0,0 +1,15 @@
+/*
+  Ví dụ 1-5. Chương trình đổi giá trị hai biến số nguyên x và y.
+  */
+#include <stdio.h>
+main()
+{
+  int x, y, tg;
+  printf("Nhap hai so nguyen ");
+  scanf("%d %d", &x, &y);
+  printf("Truoc khi doi x = %d, y = %d \n",x,y);
+  tg = x;
+  x = y;
+  y = tg;
+  printf("Sau khi doi x = %d, y = %d \n",x,y);
+}
