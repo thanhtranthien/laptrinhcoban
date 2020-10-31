53f6fae32ba8f4cf04e2e437a70606d8b978977a
thanhtranthien <50793089+thanhtranthien@users.noreply.github.com>
2020-10-31 22:32:52 +0700

    Update Vidu1-2.c

--- Bài 1/Vidu1-2.c
+++ Bài 1/Vidu1-2.c
@@ -1,1 +1,11 @@
-
+/*
+  Ví dụ 1-2. Chương trình nhập 1 ký tự in mã của ký tự đó.
+*/
+#include <stdio.h>
+main()
+{
+  char c;
+  printf("Nhap 1 ky tu");
+  scanf("%c", &c);
+  printf("Ma cua ky tu %c la: %d", c, c);
+}
